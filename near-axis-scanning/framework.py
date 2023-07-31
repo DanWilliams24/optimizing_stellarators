816801808300d09375c6867447bebb3a82b87b30
DanWilliams24 <dwillia2@umbc.edu>
2023-07-31 13:46:14 -0400

    added vmec, bnorm, regcoil support to full framework

--- near-axis-scanning/framework.py
+++ near-axis-scanning/framework.py
@@ -1,7 +1,7 @@
-
 import subprocess
 import desc
 import time
+from qsc_config import QSConfig
 from datetime import datetime
 from qsc import Qsc
 import numpy as np
@@ -100,15 +100,99 @@ def convert_to_vmec(desc_eq):
     output_name = f"./SOLOVEV_output_{timestamp}.nc"
     VMECIO.save(desc_eq, output_name)
     #We now have a file SOLOVEV_output.nc which is the equivalent Equilibrium solution in the VMEC coordinates and data format.
-    return output_name
+    return output_name[2:]
 
+
+# Given a output VMEC file, get the Normal B field distribution
 def calculate_bnorm(bnorm_input):
-    pass
+    new_bnorm_input = f"wout.{bnorm_input}"
+    test = subprocess.run(["mv", f"{bnorm_input}", new_bnorm_input])
+    test = subprocess.run(["bash", "-c","source ~/scripts/load_stellopt.sh"])
+    test = subprocess.run(["xbnorm", new_bnorm_input])
+
+    test = subprocess.run(["bash", "-c", "source ~/scripts/load_desc.sh"])
+    test = subprocess.run(["bash", "-c","source ~/scripts/load_qsc.sh"])
+    test = subprocess.run(["bash", "-c", "source ~/scripts/load_regcoil.sh"])
+    return new_bnorm_input
+
+def generate_regcoil_input_file(wout, bnorm, nescin, out):
+    regcoil_config = QSConfig("regcoil_in.template")
+    regcoil_config['wout_filename'] = wout
+    regcoil_config['bnorm_filename'] = bnorm
+    regcoil_config['nescin_filename'] = nescin
+    input_filename = f"regcoil_in.{out}"
+    regcoil_config.write_input_file(input_filename)
+    #set any other relevant params
+    return input_filename
+
+def compute_regcoil(inputfile):
+    test = subprocess.run(["regcoil", f"{inputfile}"])
+
+
+    #all we need is the regcoil output format
+    return ""
 
 def main():
-    pass
+    print("State: Near Axis Solve")
+    start_time = time.time()
+    qsc_output = near_axis_solve("qsc_in.random_scan_small")
+    end_time = time.time()
+    count_positive_time = end_time - start_time
+
+    
+    print("State: Converting to DESC")
+    desc_eq = convert_to_desc_eq(qsc_output)
+
+    print("State: Saving as VMEC")
+    vmec_filename = convert_to_vmec(desc_eq=desc_eq)
+
+    basename = vmec_filename[:-3]
+    print("State: Calculating BNORM")
+    wout_bnorm_file = calculate_bnorm(vmec_filename)
+
+    print("State: Computing REGCOIL")
+    regcoil_in = generate_regcoil_input_file(wout=wout_bnorm_file, bnorm= f"bnorm.{basename}",nescin=f"nescin.{basename}", out=basename)
+    outputfile = compute_regcoil(regcoil_in)
+
+def mainv2():
+    print("State: Near Axis Solve")
+    start_time = time.time()
+    qsc_output = near_axis_solve("qsc_in.multiopt_QA_nfp2")
+    end_time = time.time()
+    count_positive_time = end_time - start_time
+    print(f"Near Axis Solve runtime: {count_positive_time:.6f} seconds")
+    
+    print("State: Converting to DESC")
+    start_time = time.time()
+    desc_eq = convert_to_desc_eq(qsc_output.replace(".nc", "_1.nc"))
+    end_time = time.time()
+    convert_to_desc_eq_time = end_time - start_time
+    print(f"Converting to DESC runtime: {convert_to_desc_eq_time:.6f} seconds")
+
+    print("State: Saving as VMEC")
+    start_time = time.time()
+    vmec_filename = convert_to_vmec(desc_eq=desc_eq)
+    end_time = time.time()
+    convert_to_vmec_time = end_time - start_time
+    print(f"Saving as VMEC runtime: {convert_to_vmec_time:.6f} seconds")
+
+    basename = vmec_filename[:-3]
+    print("State: Calculating BNORM")
+    start_time = time.time()
+    wout_bnorm_file = calculate_bnorm(vmec_filename)
+    end_time = time.time()
+    calculate_bnorm_time = end_time - start_time
+    print(f"Calculating BNORM runtime: {calculate_bnorm_time:.6f} seconds")
+
+    print("State: Computing REGCOIL")
+    start_time = time.time()
+    regcoil_in = generate_regcoil_input_file(wout=wout_bnorm_file, bnorm=f"bnorm.{basename}", nescin=f"nescin.{basename}", out=basename)
+    outputfile = compute_regcoil(regcoil_in)
+    end_time = time.time()
+    compute_regcoil_time = end_time - start_time
+    print(f"Computing REGCOIL runtime: {compute_regcoil_time:.6f} seconds")
 
 
 
 if __name__ == "__main__":
-    main()
+    mainv2()
